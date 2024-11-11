bca6d151314092082e852e357915a5fdda73c042
Fernando <fernandoasfilho74@gmail.com>
2024-11-11 01:01:41 -0300

    ISimpressed

--- Services/Services/PortfolioService.py
+++ Services/Services/PortfolioService.py
@@ -5,8 +5,8 @@ class PortfolioService:
     @staticmethod
     def create_portfolio(cursor, person_id: int, image_ids: List[str], authentication_id: str) -> bool:
         for i, image_id in enumerate(image_ids):
-            cursor.execute("""INSERT INTO Portfolio (PersonId, ImageId, AuthenticationId) 
-                            VALUES (%s, %s, %s)""",
+            cursor.execute("""INSERT INTO Portfolio (PersonId, ImageId, AuthenticationId, IsImpressed) 
+                            VALUES (%s, %s, %s, 0)""",
                            (person_id, image_id, authentication_id,))
 
             if cursor.rowcount <= 0:
